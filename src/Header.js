475c7f8cdf7a141513eef46a1631a9ad48be167b
ArrfanD <007arfan@gmail.com>
2022-04-18 11:54:56 +0530

    commit to include full react project

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+// import SearchIcon from '@material-ui/icons/Search';
+// import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
 
@@ -19,7 +19,8 @@ function Header(props) {
                     
                     <div className='header__search'>
                         <input className='header__searchInput' type='text'/>
-                        <SearchIcon className='header__searchIcon'/>
+                        <img src='https://www.linkpicture.com/q/search_iconj.jpg'></img>
+                        {/* <SearchIcon className='header__searchIcon'/> */}
                     </div>
                     <div className='header__nav'>
                           <Link to={'/login'}>
@@ -38,7 +39,8 @@ function Header(props) {
                         </div>
                         <div className='header__optionBasket'>
                           <Link to={'/checkout'} className="header__optionBasket__link">
-                                <ShoppingBasketIcon className='header__basketIcon'/>
+                                <img src="https://www.linkpicture.com/q/cart_icon.jpg" className="header__optionBasket__link"></img>
+                                {/* <ShoppingBasketIcon className='header__basketIcon'/> */}
                           </Link>
                               <span className='header__optionLineTwo header__basketCount'>{basket?.length}</span>
                               
